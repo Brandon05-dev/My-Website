c90856b238b2ddd2e0bc29ffdbf9dc74acab88e1
Brandon <omutitibrandon@gmail.com>
2025-05-12 23:19:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slideIn = document.querySelector(".slide-in");
+    slideIn.style.animation = "slideIn 1.5s ease-out forwards";
+  });
+  
